dcffcff01ca3c41ca82c809314307caf0187cf11
sohan9819 <sohanshetty2001@gmail.com>
2021-09-01 23:19:49 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const inputArea = document.querySelector("#inputText");
+const translateBtn = document.querySelector('#submit');
+const outputArea = document.querySelector('#outputText');
+
+translateBtn.addEventListener('click' , function(e){
+    e.preventDefault();
+    let url = Url(inputArea.value);
+    if ( inputArea.value === ""){
+        outputArea.value = ""
+    }else {
+        translate(url);
+    }
+});
+
+const translate = (url) => {
+    fetch(url)
+    .then (res => {
+        return res.json()
+    })
+    .then(data => {
+        // console.log("Data Parsed");
+        outputArea.value = data.contents.translated;
+    })
+    .catch(err => {
+        alert("Opps Error!!! \nPlease try again after sometime")
+        console.log("Error!!!!" , err)
+    })
+}
+
+const Url = (text) => {
+    return 'https://api.funtranslations.com/translate/mandalorian.json' + `"${text}"`;
+}
+
+inputArea.addEventListener('click' , function(){
+    // console.dir(outputArea)
+    outputArea.value="";
+})
